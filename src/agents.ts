192061472f7577048ba981bf8e0238588b7d0cba
Darian Hogue <hogued96@gmail.com>
2022-10-06 15:54:19 -0400

    updated eslint and added playable and non playable character methods and tests

--- src/agents.ts
+++ src/agents.ts
@@ -1,14 +1,23 @@
 import axios from 'axios';
+
 const apiURL = 'https://valorant-api.com/v1/agents';
 
-export function getAllAgents(isPlayableCharacter: boolean) {
+export function getAllAgents(isPlayableCharacter?: boolean) {
   return new Promise((resolve, reject) => {
-    axios.get(apiURL)
-      .then((res) => {
-        resolve(res.data.data);
-      })
-      .catch(reject);
+    if (isPlayableCharacter === undefined) {
+      axios.get(apiURL)
+        .then((res) => {
+          resolve(res.data.data);
+        })
+        .catch(reject);
+    } else {
+      axios.get(apiURL, { params: { isPlayableCharacter } })
+        .then((res) => {
+          resolve(res.data.data);
+        })
+        .catch(reject);
+    }
   });
-};
+}
 
-export default {getAllAgents};
+export default { getAllAgents };
--- src/tests/agent.test.ts
+++ src/tests/agent.test.ts
@@ -1,20 +1,45 @@
 const axios = require('axios');
-const sdk = require('../agents');
-
+const sdk = require('../agents.ts');
 
 const apiURL = 'https://valorant-api.com/v1/agents';
 
 describe('Agent Tests', () => {
-  test('Should get all agent data', ()=> {
+  test('Should get all agent data', () => {
     return sdk.getAllAgents()
-    .then((data: any) => {
-      return axios.get(apiURL)
-      .then((res: any) => {
-        expect(data).toMatchObject(res.data.data);
+      .then((data: any) => {
+        return axios.get(apiURL)
+          .then((res: any) => {
+            expect(data).toMatchObject(res.data.data);
+          });
       })
-    })
-    .catch((err: any) => {
-      expect(err.message).toBe('No Error');
-    })
-  })
+      .catch((err: any) => {
+        expect(err.message).toBe('No Error');
+      });
+  });
+
+  test('Should get all playable agents', () => {
+    return sdk.getAllAgents(true)
+      .then((data: any) => {
+        return axios.get(apiURL, { params: { isPlayableCharacter: true } })
+          .then((res: any) => {
+            expect(data).toMatchObject(res.data.data);
+          })
+          .catch((err: any) => {
+            expect(err.message).toBe('No Error');
+          });
+      });
+  });
+
+  test('Should get all non-playable agents', () => {
+    return sdk.getAllAgents(false)
+      .then((data: any) => {
+        return axios.get(apiURL, { params: { isPlayableCharacter: false } })
+          .then((res: any) => {
+            expect(data).toMatchObject(res.data.data);
+          })
+          .catch((err: any) => {
+            expect(err.message).toBe('No Error');
+          });
+      });
+  });
 });
